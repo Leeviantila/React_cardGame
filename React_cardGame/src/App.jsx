c28ee99501e7f62f3db5b862856231fa560543f2
Leeviantila <54677074+Leeviantila@users.noreply.github.com>
2023-09-07 19:27:41 +0300

    Pohja projektilla. Nettisivu pohja tehty ja kortti komponentti aloitettu

---
+++ React_cardGame/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./components/Card";
+
+export default function App(){
+
+  return(
+    <Card/>
+  
+    );
+}
+
+
+
+
+
+
+
---
+++ React_cardGame/src/components/Card.jsx
@@ -0,0 +1,20 @@
+
+export default function Card(){
+
+    return(
+
+        <div>
+            <img src = "http://placekitten.com/120/100"/>
+            <span>Cuteness</span>
+            <span></span>
+
+        </div>
+    );
+}
+
+
+
+
+
+
+
