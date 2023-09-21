0ccac793fef6a5e45f891104a1a1a007b39f54cc
Leeviantila <54677074+Leeviantila@users.noreply.github.com>
2023-09-21 19:28:18 +0300

    Vastustajan kortti ja pohja vertailu napille

--- React_cardGame/src/App.jsx
+++ React_cardGame/src/App.jsx
@@ -1,17 +1,51 @@
 import Card from "./components/Card";
 import "./App.css"
 
-const playerCard = {
-  image: "http://placekitten.com/120/101",
-  stats: [{name: "Cuteness", value: 10}]
 
+const playerCard = {
+  image: "http://placekitten.com/120/100?image=2",
+  stats: [{name: "Cuteness", value: 10},
+          {name: "Speed", value: 50}]
 }
+
+const opponentCard = {
+  image: "http://placekitten.com/120/104?image=6",
+  stats: [{name: "Cuteness", value: 10},
+          {name: "Speed", value: 1}]
+}
+
 
 
 export default function App(){
 
+  function difference_print(){
+    const playerStats = playerCard.stats[0];
+    const opponentSats = opponentCard.stats[0];
+    let result = '';
+
+    if(playerStats.value > opponentSats.value){
+      result = 'pelaaja';
+    }
+
+    else if(playerStats.value < opponentSats.value){
+      result = 'vastustaja';
+    }
+
+    else if(playerStats.value === opponentSats.value){
+      result = 'Tasa peli';
+    }
+
+    console.log(result);
+
+  }
+
+
   return(
-    <Card card={playerCard}/>
+    <div>
+      <Card card={playerCard}/>
+      <Card card={opponentCard}/>
+      <button onClick={difference_print} type ='button'>Play</button>
+    </div>
   
     );
 }
--- React_cardGame/src/components/Card.jsx
+++ React_cardGame/src/components/Card.jsx
@@ -6,12 +6,17 @@ export default function Card({card}){
         <div className="card">
             <img src = {card.image}/>
 
-            <ul className="stat-list">
-                <li className="stat-list-item">
-                    <span>Cuteness</span>
-                    <span>3</span>
+            <ul className="stat-list">         
+                {card.stats.map((stat, index) => (
+
+
+                <li className="stat-list-item" key={index}>
+                    <span>{stat.name}</span>
+                    <span>{stat.value}</span>
 
                 </li>
+
+                ))}
 
 
             </ul>
