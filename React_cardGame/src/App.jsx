4d1460cdbf569537a9f59f0cd9b7bd5f2d7b5801
Leeviantila <54677074+Leeviantila@users.noreply.github.com>
2023-09-28 19:26:51 +0300

    Korttien asettelua päivitetty. Yleinen kortin luomispohja aloitettu.

--- React_cardGame/src/App.jsx
+++ React_cardGame/src/App.jsx
@@ -1,38 +1,53 @@
 import Card from "./components/Card";
 import "./App.css"
+import { useState } from "react";
 
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 
 const playerCard = {
   image: "http://placekitten.com/120/100?image=2",
-  stats: [{name: "Cuteness", value: 10},
-          {name: "Speed", value: 50}]
+  stats: [{name: "Cuteness", value: getRandomInt(1, 999)},
+          {name: "Speed", value: getRandomInt(1, 999)}]
 }
 
 const opponentCard = {
   image: "http://placekitten.com/120/104?image=6",
-  stats: [{name: "Cuteness", value: 10},
-          {name: "Speed", value: 1}]
+  stats: [{name: "Cuteness", value: 1},
+          {name: "Speed", value: 100}]
 }
 
+const createCard = index => ({
+  image: "http://placekitten.com/120/100?image=" + index,
+  stats: [{name: "Cuteness", value: getRandomInt(1, 999)},
+          {name: "Speed", value: getRandomInt(1, 999)},
+          {name: "Speed", Weight: getRandomInt(1, 999)}
+        ],
+
+  id: crypto.randomUUID()
+
+
+
+})
 
 
 export default function App(){
 
+  const [result, setResult] =useState('');
+
   function difference_print(){
     const playerStats = playerCard.stats[0];
     const opponentSats = opponentCard.stats[0];
-    let result = '';
 
     if(playerStats.value > opponentSats.value){
-      result = 'pelaaja';
+      setResult('Voitit');
     }
 
     else if(playerStats.value < opponentSats.value){
-      result = 'vastustaja';
+      setResult('Hävisit');
     }
 
     else if(playerStats.value === opponentSats.value){
-      result = 'Tasa peli';
+      setResult('Tasa peli');
     }
 
     console.log(result);
@@ -41,11 +56,26 @@ export default function App(){
 
 
   return(
-    <div>
-      <Card card={playerCard}/>
-      <Card card={opponentCard}/>
-      <button onClick={difference_print} type ='button'>Play</button>
-    </div>
+    <>
+      <h1>Kissa kortti peli</h1>
+
+      <div className="game">
+        <Card card={playerCard}/>
+
+        <div className="center-area">
+          <p>{result || 'Paina nappia'}</p>
+          <button onClick={difference_print} type ='button'>Play</button>
+        
+        </div>
+        <Card card={opponentCard}/>
+
+      </div>
+
+
+
+    
+    </>
+
   
     );
 }
